839ab999ad5d410dcd4989a81cd6d02c4150b9d6
Matt Atkins <matt@yoomee.com>
2011-07-20 14:20:43 +0100

    Initial Commit

---
+++ Picnic/RootViewController.h
@@ -0,0 +1,22 @@
+//
+//  RootViewController.h
+//  Picnic
+//
+//  Created by Matthew Atkins on 20/07/2011.
+//  Copyright 2011 Yoomee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ Picnic/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Picnic
+//
+//  Created by Matthew Atkins on 20/07/2011.
+//  Copyright 2011 Yoomee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PicnicAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([PicnicAppDelegate class]));
+    }
+    return retVal;
+}
