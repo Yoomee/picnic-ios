dc11af2a52d4ec721cd11f7a04a2eb9500c74710
Ian Mooney <ian@yoomee.com>
2011-07-21 14:09:02 +0100

    Changed "Venues" label to "Sessions"

--- Picnic/DetailViewController.m
+++ Picnic/DetailViewController.m
@@ -133,7 +133,7 @@
 
 - (void)splitViewController:(UISplitViewController *)svc willHideViewController:(UIViewController *)aViewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController: (UIPopoverController *)pc
 {
-    barButtonItem.title = @"Venues";
+    barButtonItem.title = @"Sessions";
     NSMutableArray *items = [[self.toolbar items] mutableCopy];
     [items insertObject:barButtonItem atIndex:0];
     [self.toolbar setItems:items animated:YES];
